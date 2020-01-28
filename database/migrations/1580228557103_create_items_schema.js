7ca7ffa320774d8abeb5a18d5508fd66f3409e77
Deluxateer <deluxateer@gmail.com>
2020-01-28 10:48:02 -0600

    add addresses, orders, cart, items, foreign-key-to-products, users migrations

--- database/migrations/1580228557103_create_items_schema.js
+++ database/migrations/1580228557103_create_items_schema.js
@@ -3,22 +3,22 @@
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 
-class CreateProductsSchema extends Schema {
+class CreateItemsSchema extends Schema {
   up () {
     this.raw(
-      `CREATE TABLE products(
+      `CREATE TABLE items(
         id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         sku VARCHAR(100) NOT NULL,
         title VARCHAR(100) NOT NULL,
-        description TEXT,
         image_url TEXT,
-        color VARCHAR(20) NOT NULL,
         price FLOAT UNSIGNED NOT NULL,
         qty INT UNSIGNED NOT NULL,
-        material VARCHAR(100) NOT NULL,
         brand_id INT UNSIGNED NOT NULL,
         type_id INT UNSIGNED NOT NULL,
         user_id INT UNSIGNED NOT NULL,
+        FOREIGN KEY(brand_id) REFERENCES brands(id),
+        FOREIGN KEY(type_id) REFERENCES types(id),
+        FOREIGN KEY(user_id) REFERENCES users(id),
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
       )
@@ -27,8 +27,8 @@ class CreateProductsSchema extends Schema {
   }
 
   down () {
-    this.raw(`DROP TABLE products`)
+    this.raw(`DROP TABLE items`)
   }
 }
 
-module.exports = CreateProductsSchema
+module.exports = CreateItemsSchema
