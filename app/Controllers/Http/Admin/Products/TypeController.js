6b797c8867aff5f82a37e40a6f258349aa6ed17d
Deluxateer <deluxateer@gmail.com>
2020-01-31 13:34:51 -0600

    loop over types for admin-product-types

--- app/Controllers/Http/Admin/Products/TypeController.js
+++ app/Controllers/Http/Admin/Products/TypeController.js
@@ -1,8 +1,24 @@
 'use strict'
 
+const Database = use('Database');
+const sanitize = use('sqlstring');
+
 class TypeController {
-  index({ view, auth }) {
-		return view.render('admin/products/types/all_types');
+  async index({ view, auth, request, response }) {
+    try {
+      let allTypes = await Database.raw(`
+        SELECT *, DATE_FORMAT(types.updated_at, '%m/%d/%Y') as updated_at FROM types
+        ORDER BY title ASC
+      `);
+
+      allTypes = allTypes[0];
+
+      return view.render('admin/products/types/all_types', { allTypes });
+    } catch(err) {
+      console.log(err);
+
+      return response.redirect('back');
+    }
 	}
 }
 
